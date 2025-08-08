d6f5377e816a5f9fc30fac3504e09ff1e52c5b3f
mouse <107972715+ocelot2024@users.noreply.github.com>
2025-08-08 16:44:34 +0900

    デプロイ時に画像が消得ていたのを修正

--- src/hq/assets/data.js
+++ src/hq/assets/data.js
@@ -1,5 +1,5 @@
 import { defineApps } from "./core";
-
+import editLogo from "../../apps/edit/assets/img/edit.svg";
 export default {
-    apps: [defineApps("Edit", "/edit.svg", "/edit")],
+    apps: [defineApps("Edit", editLogo, "/edit")],
 };
