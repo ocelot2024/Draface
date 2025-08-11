d62ed9d5328816efa8682207f6429dcf75f2f79a
ocelot2024 <ocelot2024@users.noreply.github.com>
2025-08-11 12:06:52 +0000

    deploy: f19edd03f4aa7fd5c0254f1147abc232c7d1d9a6

--- assets/ja-jp-BcZuqGMF.js
+++
@@ -1,1 +0,0 @@
-const a={lang:"ja-jp",Features:{file:"ファイルの読み書き"}};export{a as default};
---
+++ assets/ja-jp-Du00x1bX.js
@@ -0,0 +1,1 @@
+const a={lang:"ja-jp",Features:{file:"ファイルの読み書き",smartPhone:"スマホ環境での使用"}};export{a as default};
