9f0143ed6f769062772529bbdb25c1c3ae5e9b6a
mouse <107972715+ocelot2024@users.noreply.github.com>
2025-09-20 11:53:49 +0900

    ボタンコンポーネント

---
+++ src/hq/hq.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Button from '@/components/base/Button.vue';
+
+</script>
+<template>
+    <Button :disabled="true">ボタン</Button>
+    <Button variant="primary">ボタン</Button>
+    <Button variant="simple">ボタン</Button>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import Hq from "@/hq/hq.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/",
+            component: Hq,
+        },
+    ],
+});
 
-export default router
+export default router;
