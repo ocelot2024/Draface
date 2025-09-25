804c7b5a6635081d7694d9bdc67b53b3dad5349b
ocelot2024 <ocelot2024@users.noreply.github.com>
2025-09-25 01:24:02 +0000

    deploy: 8d84bd80931231e030a1951e41698a5ff1321f4a

---
+++ assets/ja-jp-DYpiKxEQ.js
@@ -0,0 +1,1 @@
+const a={brand:{normal:"Neffice"},hq:{navigation:{to_home:"ホーム",to_recent:"最近の項目"}}};export{a as default};
--- assets/ja-jp-Du00x1bX.js
+++
@@ -1,1 +0,0 @@
-const a={lang:"ja-jp",Features:{file:"ファイルの読み書き",smartPhone:"スマホ環境での使用"}};export{a as default};
