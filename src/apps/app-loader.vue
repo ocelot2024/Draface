1476974cc5b718e27c52f7687aae7a21b3a25dfa
mouse <107972715+ocelot2024@users.noreply.github.com>
2025-09-28 09:02:02 +0900

    アプリが存在しないときはHQに戻る

--- src/apps/app-loader.vue
+++ src/apps/app-loader.vue
@@ -1,4 +1,21 @@
-<script setup></script>
+<script setup>
+import { useRoute } from 'vue-router';
+import apps from './apps';
+import router from '@/router';
+const appData = apps.find((v) => useRoute().params.id == v.id);
+if (!appData) router.replace('/')
+</script>
 <template>
-    <h1>app!!</h1>
+    <div class="loading-wrapper">
+        <img :src="appData.icon" width="80">
+    </div>
 </template>
+<style scoped>
+.loading-wrapper {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
