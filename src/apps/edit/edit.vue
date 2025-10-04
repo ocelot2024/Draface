51a02150821bc5c798e110476897e78c7b1a26e1
mouse <107972715+ocelot2024@users.noreply.github.com>
2025-10-04 19:05:16 +0900

    エディタの見た目だけ

--- src/apps/edit/edit.vue
+++ src/apps/edit/edit.vue
@@ -1,2 +1,15 @@
-<script setup></script>
-<template></template>
+<script setup>
+import SidePanelViewContainer from '@/components/layouts/SidePanelViewContainer.vue';
+import Editor from './components/editor.vue';
+
+</script>
+<template>
+    <SidePanelViewContainer>
+        <template #pane-item>
+            <h1>Edit</h1>
+        </template>
+        <template #content>
+            <Editor />
+        </template>
+    </SidePanelViewContainer>
+</template>
