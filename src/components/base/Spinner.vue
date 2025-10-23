60fae367a95f88cf2a8604dcd50483f7b2073019
mouse <107972715+ocelot2024@users.noreply.github.com>
2025-10-23 20:23:37 +0900

    Create Spinner.vue

---
+++ src/components/base/Spinner.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+<template>
+    <div class="spinner-container">
+        <div class="spinner"></div>
+        <div class="spinner"></div>
+        <div class="spinner"></div>
+        <div class="spinner"></div>
+        <div class="spinner"></div>
+        <div class="spinner"></div>
+        <div class="spinner"></div>
+        <div class="spinner"></div>
+    </div>
+</template>
+<style scoped></style>
