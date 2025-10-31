c1fe59708fc7a78526ae23b72fff322127c78c55
ocelot2024 <ocelot2024@users.noreply.github.com>
2025-10-31 00:32:19 +0000

    deploy: ef040369fdd69fcb62a0248456bd14138c3ac7b2

--- assets/ja-jp-59DK8Enw.js
+++
@@ -1,1 +0,0 @@
-const a={brand:{normal:"Draface"},hq:{navigation:{to_home:"ホーム",to_recent:"最近の項目",install_pwa:"PWAをインストール",about:"Drafaceについて"},home:{home:"ホーム",apps:"アプリ"},about:{describe:"シンプルなオフィススイート"}}};export{a as default};
