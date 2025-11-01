4f360abb267d574a36afea15b9c0232aeb34e188
mouse <107972715+ocelot2024@users.noreply.github.com>
2025-11-01 16:27:27 +0900

    グローバル変数にブランチ情報を登録しておく

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
+import { execSync } from "node:child_process";
+
+const isDev = process.env.NODE_ENV !== "production";
+
+const branch = isDev
+    ? execSync("git rev-parse --abbrev-ref HEAD").toString().trim()
+    : "deploy";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,5 +22,8 @@ export default defineConfig({
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
+    },
+    define: {
+        __BRANCH__: JSON.stringify(branch),
     },
 });
