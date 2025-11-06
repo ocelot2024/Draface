85edb10c0b9979bae22343f732c54afed2aacaaa
ocelot2024 <ocelot2024@users.noreply.github.com>
2025-11-06 00:26:59 +0000

    deploy: fceeeccde45c1a29e8e3338e1aab37228a2da975

---
+++ assets/ja-jp--f0tEilE.js
@@ -0,0 +1,1 @@
+const a={brand:{normal:"Draface"},hq:{navigation:{to_home:"ホーム",install_pwa:"PWAをインストール",about:"Drafaceについて"},home:{home:"ホーム",recent_items:"最近使った項目",apps:"アプリ"},about:{describe:"シンプルなオフィススイート"}},edit:{menubar:{file:{label:"ファイル",start_with_new:"新規",save_to_local:"コンピュータに保存...",quit:"閉じる"}},file:{default_name:"名称未設定"}}};export{a as default};
