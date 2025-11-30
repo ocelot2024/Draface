8fb39bc89e038f75cfd65878f33e338f701f7839
mouse <107972715+ocelot2024@users.noreply.github.com>
2025-11-30 09:52:38 +0900

    改行がおかしかった

--- src/apps/edit/js/editor.js
+++ src/apps/edit/js/editor.js
@@ -1,38 +1,54 @@
 export default class Editor {
     constructor() {
         this.data = "";
-        this.editorArea = "";
+        this.editorArea = null;
         this.isCompositing = false;
-
         this.onKeydown = this.onKeydown.bind(this);
         this.onCompositionStart = this.onCompositionStart.bind(this);
         this.onCompositionEnd = this.onCompositionEnd.bind(this);
     }
+
     onKeydown(e) {
         const keyCode = e.code;
         const key = e.key;
-        const value = this.editorArea.value.innerText;
-        if (keyCode == "Enter" && !this.isCompositing) {
+        const value = this.editorArea.value || this.editorArea.innerText;
+
+        if (keyCode === "Enter" && !this.isCompositing) {
             e.preventDefault();
             const selection = window.getSelection();
-            const range = selection.getRangeAt(0).endOffset;
-            this.editorArea.value.innerText =
-                value.substring(0, range) + "\n" + value.substring(range);
+            const range = selection.getRangeAt(0);
+            const position = range.endOffset;
+            if (this.editorArea instanceof HTMLTextAreaElement) {
+                this.editorArea.value =
+                    value.substring(0, position) +
+                    "\n" +
+                    value.substring(position);
+            } else {
+                this.editorArea.innerText =
+                    value.substring(0, position) +
+                    "\n" +
+                    value.substring(position);
+            }
         }
     }
+
     onCompositionStart() {
         this.isCompositing = true;
     }
+
     onCompositionEnd() {
         this.isCompositing = false;
     }
-    setupEditor() {
-        this.editorArea.value.addEventListener("keydown", this.onKeydown);
-        this.editorArea.value.addEventListener(
+
+    setupEditor(editorElement) {
+        this.editorArea = editorElement;
+
+        this.editorArea.addEventListener("keydown", this.onKeydown);
+        this.editorArea.addEventListener(
             "compositionstart",
             this.onCompositionStart
         );
-        this.editorArea.value.addEventListener(
+        this.editorArea.addEventListener(
             "compositionend",
             this.onCompositionEnd
         );
